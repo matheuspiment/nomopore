6d3be34067b74da2f82a60867e1c1956db28ff4b
Matheus Ribeiro Pimenta Nunes <matheuspiment@hotmail.com>
2020-04-09 14:17:06 +0100

    add change in one of the packages

--- packages/endpoint-circle/handler.js
+++ packages/endpoint-circle/handler.js
@@ -1,18 +1,18 @@
-// const {
-//   databaseManager,
-// } = require("@matheuspiment/nomopore-common/components/database");
+const {
+  databaseManager,
+} = require("@matheuspiment/nomopore-common/components/database");
 
 module.exports.createCircle = async (event, context) => {
-  // console.log("connection", databaseManager.connection);
+  console.log("connection", databaseManager.connection);
 
-  // await databaseManager.connect();
+  await databaseManager.connect();
 
-  // console.log("connection", databaseManager.connection);
+  console.log("connection", databaseManager.connection);
 
   return {
     statusCode: 201,
     body: JSON.stringify({
-      created: true,
+      created: databaseManager.connection,
     }),
   };
 };
